fb56c37096dcb4049b70b21fc3f369e744424036
pedroffd <pedro.alcarin@gmail.com>
2022-02-23 22:00:51 -0400

    navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { RIMenuLine, RICloseLin } from 'react-icons/ri'
+import './navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+  <div className='gpt3__navbar'>
+    <div className='gpt3__navbar-links'>
+      <div className='gpt3__navbar-links_logo'>
+        <img src={logo} alt="logo"/>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='home'>Home</a></p>
+          <p><a href='wgpt3'>What is gpt3</a></p>
+          <p><a href='possibility'>Open AI</a></p>
+          <p><a href='featrures'>Case Studies</a></p>
+          <p><a href='blog'>Library</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Navbar;
