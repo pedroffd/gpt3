2de4425233dccc52d86f9c50090f93e5233857bf
pedroffd <pedro.alcarin@gmail.com>
2022-03-29 19:56:11 -0400

    header base structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-
 import logo from '../../assets/logo.svg'
 import './navbar.css';
 
-const Menu = () => {
+/* const Menu = () => {
   return(
   <>
+  <div className="gpt3__navbar-links_container">
    <p><a href="#home">Home</a></p>
     <p><a href="#wgpt3">What is GPT3?</a></p>
     <p><a href="#possibility">Open AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
-  </>)
-}
+    </div>
+  </>
+  )
+} */
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -21,10 +24,14 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt="logo"/>
+          <img src={logo} alt='logo'/>
         </div>
         <div className="gpt3__navbar-links_container">
-        <Menu />
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -38,13 +45,17 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-          <Menu />
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
           <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
             <button type="button">Sign up</button>
           </div>
         </div>
-      </div>
         )}
       </div>
     </div>
